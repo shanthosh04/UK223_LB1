de1ce477865c4e03236438a867c8643e1b8806b2
shanthosh04 <shanthosh1610@gmail.com>
2024-01-28 16:48:00 +0100

    add login /register frontend

--- server/database/schema.ts
+++ server/database/schema.ts
@@ -1,7 +1,9 @@
 const USER_TABLE = `
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
 `
